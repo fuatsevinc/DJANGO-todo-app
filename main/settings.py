481ccdd55ff5eeeb39d77cc03b14820716cd56a3
fuatsevinc <fuatsevinc@icloud.com>
2022-08-31 00:21:49 +0200

    heroku url added

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/fuattodo.git']
 
 
 # Application definition
